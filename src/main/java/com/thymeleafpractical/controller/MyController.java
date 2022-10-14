34a842922b0b7286c567b1a7a1685b6732051a8c
Ramshek <ramshekrana@gmail.com>
2022-10-14 21:54:12 +0530

    mood offf

--- src/main/java/com/thymeleafpractical/controller/MyController.java
+++ src/main/java/com/thymeleafpractical/controller/MyController.java
@@ -2,10 +2,12 @@ package com.thymeleafpractical.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class MyController {
@@ -13,6 +15,7 @@ public class MyController {
     //http://localhost:8080/about
 
     //@GetMapping("/about")
+
     @RequestMapping(value = "/about",method = RequestMethod.GET)
     public String about(Model model) {
         System.out.println("Inside about handler...");
@@ -21,5 +24,13 @@ public class MyController {
         model.addAttribute("currentDate", new Date().toLocaleString());
         return "about";
     }
+
+    @GetMapping("/example-loop")
+    public String iterateHandler(Model model){
+        List<String> names = List.of("Ankit", "Laxmi", "Karan", "John");
+        model.addAttribute("name", names);
+        return "iterate";
+    }
+
 
 }
